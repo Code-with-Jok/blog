a32bf169f078da72fd920867c38d44771f89e130
o1key222k <o1key222k@gmail.com>
2026-01-30 14:56:44 +0700

    feat: implement server initialization with core dependencies, Cloudinary upload middleware, and client navigation data.

--- client/src/utils/data.ts
+++ client/src/utils/data.ts
@@ -4,7 +4,6 @@ import {
   LuLayoutDashboard,
   LuLayoutTemplate,
   LuMessageSquareQuote,
-  LuTag,
 } from "react-icons/lu";
 
 export const SIDE_MENU_DATA: SideMenuData[] = [
@@ -35,16 +34,16 @@ export const BLOG_NAVBAR_DATA: BlogNavbarData[] = [
     icon: LuLayoutTemplate,
     path: "/",
   },
-  {
-    id: "02",
-    label: "React JS",
-    icon: LuTag,
-    path: "/tag/react-js",
-  },
-  {
-    id: "03",
-    label: "Next JS",
-    icon: LuTag,
-    path: "/tag/next-js",
-  },
+  // {
+  //   id: "02",
+  //   label: "React JS",
+  //   icon: LuTag,
+  //   path: "/tag/react-js",
+  // },
+  // {
+  //   id: "03",
+  //   label: "Next JS",
+  //   icon: LuTag,
+  //   path: "/tag/next-js",
+  // },
 ];
